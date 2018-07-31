c58fc8fde8f5b1075cc0b6f5a6f4de2b4b9503bf
APAC\motevre <reza.motevallizadeh@linkgroup.com>
2018-07-31 16:42:03 +1000

    Init(0)

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+        "encoding/base64"
+        "github.com/aws/aws-lambda-go/lambda"
+        "github.com/aws/aws-sdk-go/aws/session"
+        "github.com/aws/aws-sdk-go/service/kms"
+		"os"
+		"log"
+)
+
+var enClinetID string = os.Getenv("ClientID")
+var enClinetSecret string = os.Getenv("ClientSecret")
+
+
+var Subscription string
+var ClientID string
+var ClientSecret string
+
+func init() {
+	Subscription = decrypt(enSubscription)
+	ClientID = decrypt(enClinetID)
+	ClientSecret = decrypt(enClinetSecret)
+}
+
+// Decrypting encrypted environment variables
+func decrypt(value string) string {
+	kmsClient := kms.New(session.New())
+	decodedBytes, err := base64.StdEncoding.DecodeString(value)
+	if err != nil {
+			panic(err)
+	}
+	input := &kms.DecryptInput{
+			CiphertextBlob: decodedBytes,
+	}
+	response, err := kmsClient.Decrypt(input)
+	if err != nil {
+			panic(err)
+	}
+	// Plaintext is a byte array, so convert to string
+	return string(response.Plaintext[:])
+}
+
+func HandleRequest() (string, error) {
+
+	log.Println("INFO: ClientID: ", ClientID)
+	log.Println("INFO: ClientSecret: ", ClientSecret)
+
+    return "Hello, World!", nil
+}
+
+func main() {
+    lambda.Start(HandleRequest)
+}
